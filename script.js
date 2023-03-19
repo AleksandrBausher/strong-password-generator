9ecdd34f336218e5b83f478cffb384835b28ec20
AleksandrBausher <bausheraleksandr@gmail.com>
2023-03-19 17:50:41 -0400

    completed functionalities and added screenshots

--- script.js
+++ script.js
@@ -2,50 +2,66 @@ var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var upperCases = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var integers = "0123456789";
 var specials = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
-var generatedPasswordString = "";
 
 var generateBtn = document.querySelector("#generate");
 
 function passwordGenerator() {
+  //string to store password
+  var generatedPasswordString = "";
+  // prompting user to make selections for everything
   var charNos = window.prompt(
     "Enter the length of the password you want (8 to 128 characters only)"
   );
-  var isLowerCase = window.prompt("Enter 'yes' if you want to include LOWERCASE letters as well");
-  var isUpperCase = window.prompt("Enter 'yes' if you want to include UPPERCASE letters as well");
-  var isNumeric = window.prompt("Enter 'yes' if you want to include NUMERIC letters as well");
-  var isSpecialChars = window.prompt("Enter 'yes' if you want to include Special chars letters as well");
-
-  
-var passwordFromChars="";
-var passwordDump= "";
-  if(charNos>8||charNos<128 || isLowerCase=="yes" || isUpperCase=="yes" || isNumeric=="yes" || isSpecialChars=="yes"){
-    if(isLowerCase == "yes"){
-        passwordFromChars +=lowerCase
+  var isLowerCase = window.prompt(
+    "Enter 'yes' if you want to include LOWERCASE letters as well"
+  );
+  var isUpperCase = window.prompt(
+    "Enter 'yes' if you want to include UPPERCASE letters as well"
+  );
+  var isNumeric = window.prompt(
+    "Enter 'yes' if you want to include NUMERIC letters as well"
+  );
+  var isSpecialChars = window.prompt(
+    "Enter 'yes' if you want to include Special chars letters as well"
+  );
+//adding selected choice in a string
+  var passwordFromChars = "";
+  if (
+    charNos > 8 ||
+    charNos < 128 ||
+    isLowerCase == "yes" ||
+    isUpperCase == "yes" ||
+    isNumeric == "yes" ||
+    isSpecialChars == "yes"
+  ) {
+    if (isLowerCase == "yes") {
+      passwordFromChars += lowerCase;
     }
-    if(isUpperCase == "yes"){
-        passwordFromChars +=upperCases
-        
+    if (isUpperCase == "yes") {
+      passwordFromChars += upperCases;
     }
-    if(isNumeric == "yes"){
-        passwordFromChars +=integers
-
+    if (isNumeric == "yes") {
+      passwordFromChars += integers;
     }
-    if(isSpecialChars == "yes"){
-        passwordFromChars +=specials
+    if (isSpecialChars == "yes") {
+      passwordFromChars += specials;
     }
-    
-    passwordDump = generatedPasswordString.charAt(
-        Math.floor(Math.random() * charNos))
 
+    //generating randowm letters from random lib
+    for (var i = 0; i < charNos; i++) {
+      generatedPasswordString += passwordFromChars.charAt(
+        Math.floor(Math.random() * passwordFromChars.length)
+      );
+    }
 
+    console.log(generatedPasswordString);
   }
 
-  return passwordDump;
+  return generatedPasswordString;
 }
 
 // Write password to the #password input
 function writePassword() {
-  // var password = generatePassword();
   var password = passwordGenerator();
   var passwordText = document.querySelector("#password");
 
